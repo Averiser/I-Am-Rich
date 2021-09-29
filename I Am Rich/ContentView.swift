72442b0c09fef66b7722a7930e4749430c3a5333
Averiser <averiser@gmail.com>
2021-09-29 15:40:18 +0300

    first commit -206 How to Build a SwiftUI App from Scratch part1

---
+++ I Am Rich/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by MyMacBook on 29.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I Am Rich")
+          .font(.system(size: 40))
+          .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+      ContentView()
+    }
+}
