c46d4a38f9dd6c004f6746043b72250dc3568f11
Averiser <averiser@gmail.com>
2021-09-30 12:13:47 +0300

    second commit - 206 How to Build a SwiftUI App from Scratch part2

--- I Am Rich/ContentView.swift
+++ I Am Rich/ContentView.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I Am Rich")
-          .font(.system(size: 40))
-          .padding()
+      ZStack {
+        Color(.systemTeal)
+          .edgesIgnoringSafeArea(.all)
+        VStack {
+          Text("I Am Rich")
+              .font(.system(size: 40))
+              .fontWeight(.bold)
+              .foregroundColor(Color.white)
+            .padding()
+          Image("diamond")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200.0, height: 200.0, alignment: .center)
+          }
+        }
     }
 }
 
